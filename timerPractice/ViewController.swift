22b3771031a042fdbf841c8d7adc601b085efa85
ohyeah1783 <kawaishinta@gmail.com>
2019-08-24 18:18:52 +0900

    Initial Commit

---
+++ timerPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timerPractice
+//
+//  Created by Shintaro Kawai on 2019/08/24.
+//  Copyright © 2019 shintaro.kawai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
